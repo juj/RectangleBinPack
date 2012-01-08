0a4fd0c35f596b97a63b0aa927d51acb94c34444
Jukka Jylänki <jujjyl@gmail.com>
2012-01-08 14:41:16 +0200

    Initial migration to git repository.

---
+++ old/RectangleBinPack.cpp
@@ -0,0 +1,120 @@
+/** @file RectangleBinPack.cpp
+	@author Jukka Jylänki
+
+	This work is released to Public Domain, do whatever you want with it.
+
+	@brief RectangleBinPack is a data structure for performing online rectangle bin packing.
+*/
+#include "RectangleBinPack.h"
+
+/** Restarts the packing process, clearing all previously packed rectangles and
+	sets up a new bin of a given initial size. These bin dimensions stay fixed during
+	the whole packing process, i.e. to change the bin size, the packing must be
+	restarted again with a new call to Init(). */
+void RectangleBinPack::Init(int width, int height)
+{
+	binWidth = width;
+	binHeight = height;
+	root.left = root.right = 0;
+	root.x = root.y = 0;
+	root.width = width;
+	root.height = height;
+}
+
+/** @return A value [0, 1] denoting the ratio of total surface area that is in use.
+	0.0f - the bin is totally empty, 1.0f - the bin is full. */
+float RectangleBinPack::Occupancy() const
+{
+	unsigned long totalSurfaceArea = binWidth * binHeight;
+	unsigned long usedSurfaceArea = UsedSurfaceArea(root);
+
+	return (float)usedSurfaceArea/totalSurfaceArea;
+}
+
+/** Recursively calls itself. */
+unsigned long RectangleBinPack::UsedSurfaceArea(const RectangleBinPack::Node &node) const
+{
+	if (node.left || node.right)
+	{
+		unsigned long usedSurfaceArea = node.width * node.height;
+		if (node.left)
+			usedSurfaceArea += UsedSurfaceArea(*node.left);
+		if (node.right)
+			usedSurfaceArea += UsedSurfaceArea(*node.right);
+
+		return usedSurfaceArea;
+	}
+
+	// This is a leaf node, it doesn't constitute to the total surface area.
+	return 0;
+}
+
+/** Running time is linear to the number of rectangles already packed. Recursively calls itself.
+	@return 0 If the insertion didn't succeed. */
+RectangleBinPack::Node *RectangleBinPack::Insert(RectangleBinPack::Node *node, int width, int height)
+{
+	// If this node is an internal node, try both leaves for possible space.
+	// (The rectangle in an internal node stores used space, the leaves store free space)
+	if (node->left || node->right)
+	{
+		if (node->left)
+		{
+			Node *newNode = Insert(node->left, width, height);
+			if (newNode)
+				return newNode;
+		}
+		if (node->right)
+		{
+			Node *newNode = Insert(node->right, width, height);
+			if (newNode)
+				return newNode;
+		}
+		return 0; // Didn't fit into either subtree!
+	}
+
+	// This node is a leaf, but can we fit the new rectangle here?
+	if (width > node->width || height > node->height)
+		return 0; // Too bad, no space.
+
+	// The new cell will fit, split the remaining space along the shorter axis,
+	// that is probably more optimal.
+	int w = node->width - width;
+	int h = node->height - height;
+	node->left = new Node;
+	node->right = new Node;
+	if (w <= h) // Split the remaining space in horizontal direction.
+	{
+		node->left->x = node->x + width;
+		node->left->y = node->y;
+		node->left->width = w;
+		node->left->height = height;
+
+		node->right->x = node->x;
+		node->right->y = node->y + height;
+		node->right->width = node->width;
+		node->right->height = h;
+	}
+	else // Split the remaining space in vertical direction.
+	{
+		node->left->x = node->x;
+		node->left->y = node->y + height;
+		node->left->width = width;
+		node->left->height = h;
+
+		node->right->x = node->x + width;
+		node->right->y = node->y;
+		node->right->width = w;
+		node->right->height = node->height;
+	}
+	// Note that as a result of the above, it can happen that node->left or node->right
+	// is now a degenerate (zero area) rectangle. No need to do anything about it,
+	// like remove the nodes as "unnecessary" since they need to exist as children of
+	// this node (this node can't be a leaf anymore).
+
+	// This node is now a non-leaf, so shrink its area - it now denotes
+	// *occupied* space instead of free space. Its children spawn the resulting
+	// area of free space.
+	node->width = width;
+	node->height = height;
+	return node;
+}
---
+++ old/RectangleBinPack.h
@@ -0,0 +1,88 @@
+/** @file RectangleBinPack.h
+	@author Jukka Jylänki
+
+	This work is released to Public Domain, do whatever you want with it.
+
+	@brief RectangleBinPack is a data structure for performing online rectangle bin packing.
+*/
+#ifndef RectangleBinPack_h
+#define RectangleBinPack_h
+
+#include "clb/Core/Ptr.h"
+
+/** Performs 'discrete online rectangle packing into a rectangular bin' by maintaining 
+	a binary tree of used and free rectangles of the bin. There are several variants
+	of bin packing problems, and this packer is characterized by:
+	- We're solving the 'online' version of the problem, which means that when we're adding
+	  a rectangle, we have no information of the sizes of the rectangles that are going to
+	  be packed after this one.
+	- We are packing rectangles that are not rotated. I.e. the algorithm will not flip
+	  a rectangle of (w,h) to be stored if it were a rectangle of size (h, w). There is no
+	  restriction conserning UV mapping why this couldn't be done to achieve better
+	  occupancy, but it's more work. Feel free to try it out.
+	- The packing is done in discrete integer coordinates and not in rational/real numbers (floats).
+
+	Internal memory usage is linear to the number of rectangles we've already packed.
+
+	For more information, see
+	- Rectangle packing: http://www.gamedev.net/community/forums/topic.asp?topic_id=392413
+	- Packing lightmaps: http://www.blackpawn.com/texts/lightmaps/default.html
+	
+	Idea: Instead of just picking the first free rectangle to insert the new rect into,
+	      check all free ones (or maintain a sorted order) and pick the one that minimizes 
+			the resulting leftover area. There is no real reason to maintain a tree - in fact 
+			it's just redundant structuring. We could as well have two lists - one for free 
+			rectangles and one for used rectangles. This method would be faster and might
+			even achieve a considerably better occupancy rate. */
+class RectangleBinPack
+{
+public:
+	/** A node of a binary tree. Each node represents a rectangular area of the texture
+	    we surface. Internal nodes store rectangles of used data, whereas leaf nodes track 
+	    rectangles of free space. All the rectangles stored in the tree are disjoint. */
+	struct Node : public clb::RefCountable
+	{
+		// Left and right child. We don't really distinguish which is which, so these could
+		// as well be child1 and child2.
+		Ptr(Node) left;
+		Ptr(Node) right;
+
+		// The top-left coordinate of the rectangle.
+		int x;
+		int y;
+
+		// The dimension of the rectangle.
+		int width;
+		int height;
+	};
+
+	/// Starts a new packing process to a bin of the given dimension.
+	void Init(int width, int height);
+
+	/// Inserts a new rectangle of the given size into the bin.
+	/** Running time is linear to the number of rectangles that have been already packed.
+		@return A pointer to the node that stores the newly added rectangle, or 0 
+			if it didn't fit. */
+	Node *Insert(int width, int height)
+	{
+		return Insert(&root, width, height);
+	}
+
+	/// Computes the ratio of used surface area.
+	float Occupancy() const;
+
+private:
+	Node root;
+
+	// The total size of the bin we started with.
+	int binWidth;
+	int binHeight;
+
+	/// @return The surface area used by the subtree rooted at node.
+	unsigned long UsedSurfaceArea(const Node &node) const;
+
+	/// Inserts a new rectangle in the subtree rooted at the given node.
+	Node *Insert(Node *node, int width, int height);
+};
+
+#endif
