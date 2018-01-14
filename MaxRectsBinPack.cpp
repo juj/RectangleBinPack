3ddd3deed7939c337e6774b7ed1ea056214af2b9
Geequlim <geequlim@gmail.com>
2018-01-14 18:25:04 +0800

    Fix resoult value didn't put into return value in MaxRectsBinPack::Insert

--- MaxRectsBinPack.cpp
+++ MaxRectsBinPack.cpp
@@ -1,5 +1,5 @@
 /** @file MaxRectsBinPack.cpp
-	@author Jukka Jyl‰nki
+	@author Jukka Jyl√§nki
 
 	@brief Implements different bin packer algorithms that use the MAXRECTS data structure.
 
@@ -112,6 +112,7 @@ void MaxRectsBinPack::Insert(std::vector<RectSize> &rects, std::vector<Rect> &ds
 			return;
 
 		PlaceRect(bestNode);
+		dst.push_back(bestNode);
 		rects.erase(rects.begin() + bestRectIndex);
 	}
 }
